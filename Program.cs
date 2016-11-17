8bffc0ab0bdadc293925179b07ae93aa75220fe0
jsakamoto <junichi.jp@gmail.com>
2016-11-17 22:35:34 +0900

    Initial Commit.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using doNET20LocalizeApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("---- Culture = en ----");
+        Console.WriteLine("expected: Hello");
+        Console.WriteLine("actula:   " + _Localize.ResourceManager.GetString("Hello", CultureInfo.GetCultureInfo("en")));
+
+        Console.WriteLine();
+
+        Console.WriteLine("---- Culture = ja ----");
+        Console.WriteLine("expected: こんにちは");
+        Console.WriteLine("actual:   " + _Localize.ResourceManager.GetString("Hello", CultureInfo.GetCultureInfo("ja")));
+    }
+}
